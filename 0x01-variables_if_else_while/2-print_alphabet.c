a034e260367778b18acae2c98655520b8d234f2b
Clintrono25 <c36clinton@gmail.com>
2022-03-14 21:50:02 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
-	putchar('\n');
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+        putchar('\n');
 
 return (0);
 }
