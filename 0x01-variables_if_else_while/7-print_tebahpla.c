b91bb7d56c2634a97af7f1db372393dd49863122
Clintrono25 <c36clinton@gmail.com>
2022-03-14 18:00:25 +0300

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print alphabets in reverse
+ *
+ * Retunr: Always (0) Sucess
+ *
+ */
+int main(void)
+{
+	char s;
+	for (s = 'z'; s >= 'a'; s--)
+	putchar(s);
+	putchar('\n');
+
+return (0);
+}
+
