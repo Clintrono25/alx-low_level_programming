0405443669d7de87c40ff424b0ee2e5db20595c1
Clintrono25 <c36clinton@gmail.com>
2022-03-14 18:03:51 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,15 @@
  *
  * print alphabets in reverse
  *
- * Retunr: Always (0) Sucess
- *
+ * Retunr: Always (0) Success *
  */
 int main(void)
 {
 	char s;
+	
 	for (s = 'z'; s >= 'a'; s--)
 	putchar(s);
+	
 	putchar('\n');
 
 return (0);
