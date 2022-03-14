40f41173b4c5e21c6ae3838c73f3094ad407d37f
Clintrono25 <c36clinton@gmail.com>
2022-03-14 17:11:08 +0300

    Base 10 numbers from 0; 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print numbers of base 10 from 0
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int x = 0;
+	for (x = 0; x < 10; x++)
+       {
+	       printf("%i", x);
+	       putchar('\n');
+       }       
+return (0);
+}
