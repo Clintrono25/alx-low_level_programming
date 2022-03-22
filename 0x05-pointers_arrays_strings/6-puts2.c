cff4847c330446c5ea108f3de5d7297c37ec62cd
Clintrono25 <c36clinton@gmail.com>
2022-03-22 12:32:33 +0300

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints pair val
+ * @str: value to eval
+ * Return: not
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
