914bcf7d3e3b8b22fcce3fe4c644733e57a187c7
Clintrono25 <c36clinton@gmail.com>
2022-03-23 21:41:21 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * print_line - Write a function that draws a straight line in the terminal.
 * @n: Integer amount of _
@@ -8,6 +8,6 @@ void print_line(int n)
 	char c;
 
 	for (c = 0; c < n; c++)
-		_putchar('_');
-	_putchar('\n');
+		putchar('_');
+	putchar('\n');
 }
