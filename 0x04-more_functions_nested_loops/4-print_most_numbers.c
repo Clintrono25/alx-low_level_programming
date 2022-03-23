bce43c25ca650c8efbb635c311da98b7985713ed
Clintrono25 <c36clinton@gmail.com>
2022-03-23 16:58:20 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,8 @@ int void print_most_numbers(void)
 {
 	char c;
 	
-	for(c = '0'; c <= '9'; c++)
-	
-		if(c != '2' && c != '4')
+	for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
 			putchar(c);		}
 	putchar('\n');
 	}
