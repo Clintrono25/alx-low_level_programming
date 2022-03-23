5a74e95d3d999ad719f1f8c55b8a7eab22980482
Clintrono25 <c36clinton@gmail.com>
2022-03-23 16:19:59 +0300

    Fix rename

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two ints
+ * @a: var to be mul
+ * @b: var to be mul
+ * Return: mul.
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+return(x);
+}
