4e0a335f1f54ebed2619b268584e5f15fc4c4522
Clintrono25 <c36clinton@gmail.com>
2022-03-23 14:09:40 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,13 +5,8 @@
 * @c: takes in a character/digit
 * Return: 1 if digit and 0 if anything else
 */
-int _isdigit(int c)
 {
-	c = 0;
-	
-	for(c = 0; c < 10; i++)
-	{
+	if (c >= '0' && c <= '9')
 	return (1);
-	}
 	return (0);
 }
