2ada66c557b846a64a0f785ff9ec653933478806
Clintrono25 <c36clinton@gmail.com>
2022-03-29 12:44:46 +0300

    Created 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * _strchr - finds char in string
+ * @s: points to c address
+ * @c: the character to look for
+ * @s: Return NULL if the character is not found
+ *
+ * Return: a pointer to the 1st occrnce of char @c in the str
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
