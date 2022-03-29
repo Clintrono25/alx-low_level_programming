87976eb0e859a027ea4acd18de8103f8f1332f77
Clintrono25 <c36clinton@gmail.com>
2022-03-29 13:03:59 +0300

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - get the lenght of a prefix sunstring
+ *
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ *
+ * Return: the number of bytes in the initial segment of @s
+ * which consist only of bytes from @accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i]; != '\0'; i++0)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accpet[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+	}
+	return (c);
+}
