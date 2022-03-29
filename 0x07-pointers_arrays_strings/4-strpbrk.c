00fa3e4ed3a919c6acf2d519875426be453171d0
Clinton Rono <c36clinton@gmail.com>
2022-03-29 23:14:59 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.>
+#include <stddef.h>
 /**
  * _strpbrk - searching a string for any of a set of bytes
  * @s: the string
@@ -10,7 +10,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i;
-	unsinged int j;
+	unsigned int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
