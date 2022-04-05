0e028a6e26d0ec96876ddc556a41cdc9628c2eef
Clinton Rono <c36clinton@gmail.com>
2022-04-06 00:07:03 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
-	if = (str == NULL)
+	if (str == NULL)
 		return (NULL);
 	i = 0;
 	while (i < size)
