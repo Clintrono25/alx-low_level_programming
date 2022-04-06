783dabb6b21d2a8ede1f3691d6db3e4dc7121df2
Clintrono25 <c36clinton@gmail.com>
2022-04-07 01:39:31 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	int n, i, ptr*
+		scanf#include <stdio.h>
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
+
