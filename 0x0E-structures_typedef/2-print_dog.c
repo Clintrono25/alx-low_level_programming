734b94389ad97b63abdce86ed86d1ee064cf50f7
Clinton Rono <c36clinton@gmail.com>
2022-04-13 01:34:13 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,18 @@
  **/
 void print_dog(struct dog *d)
 {
-        if (d == NULL)
-                ;
-        else
-        {
-                if (d->name != NULL)
-                        printf("Name: %s\n", d->name);
-                else
-                        printf("Name: (nil)\n");
-                printf("Age: %f\n", d->age);
-                if (d->owner != NULL)
-                        printf("Owner: %s\n", d->owner);
-                else
-                        printf("Owner: (nil)\n");
-        }
+	if (d == NULL)
+		;
+	else
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
 }
