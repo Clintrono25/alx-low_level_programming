175714dcede5edaac601facd2b9d535dee56c0f8
Clinton Rono <c36clinton@gmail.com>
2022-04-14 00:58:55 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,6 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _CALC_H
+#define _CALC_H
+
 /**
  * struct op - Struct op
  *
@@ -11,10 +12,14 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int);
-#endif
+
+
+#endif /* CALC_H */
