376357517f09063beda1f8ae6761e1a37e215d73
Clinrono25 <c36clinton@gmail.com>
2022-04-22 01:52:50 +0300

    Created 4-free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * free_list - free a linked list
+ * @head: the befinning of the list
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
