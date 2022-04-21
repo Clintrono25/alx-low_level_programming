05deefc30df946f4a377361084383381848ab315
Clinton Rono <c36clinton@gmail.com>
2022-04-22 01:55:34 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * free_list - free a linked list
- * @head: the befinning of the list
+ * free_list - frees a linked list
+ * @head: the beginning of the list
  */
 void free_list(list_t *head)
 {
