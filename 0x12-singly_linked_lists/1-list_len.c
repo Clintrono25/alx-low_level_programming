31533b4dcaab672f035b4fd5b1625beea15e1fac
Clinton Rono <c36clinton@gmail.com>
2022-04-22 01:36:24 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-/*
- * list_len - returns numeber of elements in a linked list
- * @h: linked list
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * list_len - finds the number of nodes in a list
+ * @h: singly linked list
  * Return: number of elements in a linked list
  */
 size_t list_len(const list_t *h)
